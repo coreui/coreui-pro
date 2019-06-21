a17f9f752df069888c5e5770ed6ac15d91c60362
mrholek <lukasz@holeczek.pl>
2019-06-21 13:24:37 +0200

    docs: update project informations

--- build/banner.js
+++ build/banner.js
@@ -1,14 +1,14 @@
 'use strict'
 
-const path  = require('path')
-const pkg   = require(path.resolve(__dirname, '../package.json'))
-const year  = new Date().getFullYear()
+const path = require('path')
+const pkg = require(path.resolve(__dirname, '../package.json'))
+const year = new Date().getFullYear()
 
 function getBanner(pluginFilename) {
   return `/*!
-  * CoreUI${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
+  * CoreUI Pro ${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
   * Copyright ${year} ${pkg.author.name}
-  * Licensed under MIT (${pkg.homepage})
+  * License (${pkg.homepage}/pro/license/)
   */`
 }
 
