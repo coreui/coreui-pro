aaf43cc52693dcdfd41008eb0fa0dffd41f47c32
mrholek <lukasz@holeczek.pl>
2020-03-30 15:58:57 +0200

    refactor: bump version to 3.1.0

--- js/src/alert.js
+++ js/src/alert.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI (v3.0.0): alert.js
+ * CoreUI (v3.1.0): alert.js
  * Licensed under MIT (https://coreui.io/license)
  *
  * This component is a modified version of the Bootstrap's alert.js
@@ -27,7 +27,7 @@ import SelectorEngine from './dom/selector-engine'
  */
 
 const NAME = 'alert'
-const VERSION = '3.0.0-rc.4'
+const VERSION = '3.1.0'
 const DATA_KEY = 'coreui.alert'
 const EVENT_KEY = `.${DATA_KEY}`
 const DATA_API_KEY = '.data-api'
