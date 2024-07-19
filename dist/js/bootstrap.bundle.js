ed6bc482c3469d83d90014cdbd51412329238d66
mrholek <lukasz@holeczek.pl>
2024-07-19 10:52:00 +0200

    release: v5.3.0

--- dist/js/bootstrap.bundle.js
+++ dist/js/bootstrap.bundle.js
@@ -1,5 +1,5 @@
 /*!
-  * CoreUI v5.2.1 (https://bs.io)
+  * CoreUI v5.3.0 (https://bs.io)
   * Copyright 2024 The CoreUI Team (https://github.com/orgs/coreui/people)
   * Licensed under MIT (https://github.com/coreui/coreui/blob/main/LICENSE)
   */
@@ -665,7 +665,7 @@
    * Constants
    */
 
-  const VERSION = '5.2.1';
+  const VERSION = '5.3.0';
 
   /**
    * Class definition
